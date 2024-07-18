6d1f839800862c7de9b7188f39cab57273df5c3a
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-18 16:00:16 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -4,3 +4,51 @@ public class Person{
   // Implement Constructor argumented
   // Include private
 }
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String address;
+
+    // Constructor
+    public Person(int id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+}
+
+    public String getAddress() {
+        return address;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
