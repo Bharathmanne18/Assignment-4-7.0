2286360c5ad8570470163bfa87406bd86fb587e9
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-18 16:35:43 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,3 +8,49 @@ public static void main(String[]args){
   // print the values
 }
 }
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        // Create Object for Person
+        // Initialize values with Constructors
+        Person person1 = new Person("Bharath", 25);
+        System.out.println("Name: " + person1.getName());
+        System.out.println("Age: " + person1.getAge());
+
+        // Create another Object for Person
+        // Assign Values with Getters & Setters
+        Person person2 = new Person("XYZ");
+        person2.setAge(22);
+        System.out.println("Name: " + person2.getName());
+        System.out.println("Age: " + person2.getAge());
+    }
+}
